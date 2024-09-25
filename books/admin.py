53adf16e0ed4a2598a7318c5b34586710fd76c45
wilkm <123@gmail.com>
2024-09-25 21:45:46 +0200

    Modul books

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from books.models import Author, Book, Category, Publisher
+
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Category)
+admin.site.register(Publisher)
+
--- books/models.py
+++ books/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name + " " + self.surname
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    ASIN = models.CharField(max_length=11)
+    publication_date = models.DateField()
+    description = models.TextField()
+    page_count = models.IntegerField()
+    # image_url = models.URLField()
+
+
+    def __str__(self):
+        return self.title
